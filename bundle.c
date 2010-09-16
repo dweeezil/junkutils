c650d1b0dacfdca0bb50b76cfab18d00013f39ad
tim <tim@onlight.com>
2010-09-16 17:02:06 +0000

    Add an optional field delimiter character to the "bundle" program.
    Also, totally re-work its logic to avoid extra separator characters between
    or following a field (the previous logic was lazy and always appended a
    separator to the output line).  Getting it "right" is a lot more complicated
    than doing it the old way.
    
    One other benefit is that the new logic will also remove runs of blank lines.

--- bundle.c
+++ bundle.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
 
-/* bundle - "Bundle" groups of lines in the input by joining together
- * consecutive lines separated by blank lines.
+/*	bundle - Concatenate consecutive nonblank input lines into single
+ *	lines separated by either a space or a separator character.
  */
 
-main() {
-	int i, len = 0;
+
+//	doline - copy a line from stdin to stdout as a single output field.
+//	If we're "in" a bundle as indicated by the "inbundle" flag, prepend
+//	a separator character to the field.
+//
+doline(int inbundle, int sep) {
+	int i, rval = 0;
 
 	while ((i = getchar()) != EOF) {
-
-		/* On a non-newline, write the character and increment the
-		 * line length counter.
-		 */
-		if (i != '\n') {
-			putchar(i);
-			++len;
-
-		/* Got a newline...
-		 */
-		} else {
-			if (len == 0)		/* separate bundles */
-				putchar('\n');
-			else {
-				putchar(' ');	/* separate lines */
-				len = 0;	/* new input line */
-			}
+		if (i == '\n')
+			return rval;
+		if (inbundle && sep) {
+			putchar(sep);
+			sep = 0;
 		}
+		putchar(i);
+		++rval;
 	}
-	if (len)
+	return rval;
+}
+
+main(int argc, char **argv) {
+	int sep;		// field seperator
+	int inbundle = 0;	// nonzero if we're "in" a bundle (i.e. have read one field of a bundle)
+	int lastlinelen = -1;	// length of previous input line
+	int thislinelen;	// length of current input line
+
+	if (argc == 2)
+		sep = argv[1][0];
+
+	while (!feof(stdin)) {
+		if (inbundle && lastlinelen == 0) {
+			putchar('\n');
+			inbundle = 0;
+		}
+		thislinelen = doline(inbundle, sep);
+		if (thislinelen > 0)
+			inbundle = 1;
+		lastlinelen = thislinelen;
+	}
+	if (inbundle)
 		putchar('\n');
 }
