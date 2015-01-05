56f38d3d45a34795ec24c9874b09f560a1611140
Tim Chase <tim@chase2k.com>
2015-01-05 12:48:11 -0600

    Megs limit can now be specified as a second argument.

--- maxsbrk.c
+++ maxsbrk.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 main(int argc, char **argv) {
-	int megs, incrmeg;
+	int megs, incrmeg, stopmeg;
 	char *x;
 	intptr_t incr;
 
@@ -13,13 +13,22 @@ main(int argc, char **argv) {
 	else
 		incrmeg = 1;
 
+	if (argc == 3)
+		stopmeg = atoi(argv[2]);
+	else
+		stopmeg = 0;
+
 	incr = incrmeg * 1024ULL * 1024ULL;
 
 	for (megs = 0 ; ; megs += incrmeg) {
 		x = sbrk(incr);
-		if (x == (void *)-1)
+		if (x == (void *)-1) {
+			printf("sbrk failed\n");
 			break;
+		}
 		memset(x, 0x55, incr);
+		if (stopmeg && megs >= stopmeg)
+			break;
 	}
 	printf("%d MiB (%d GiB)\n", megs, megs / 1024);
 }
