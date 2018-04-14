e910c90df25637464ab4dabe38460da922b8103c
Tim Chase <tim@chase2k.com>
2018-04-14 18:53:39 -0500

    Use "git show" rather than "git diff".

--- makepatch-illumos.sh
+++ makepatch-illumos.sh
@@ -2,7 +2,7 @@
 
 files=$(git show --name-status $1 | grep '^[A-Z]	' | sed 's;^..;;')
 for file in $files; do
-	git diff "$1"^.."$1" $file > $file.patch
+	git show "$1" -- $file > $file.patch
 done
 patchfiles=$(echo $files | tr ' ' '\012' | sed -e 's;$;.patch;')
 tar cf patch.tar $patchfiles
